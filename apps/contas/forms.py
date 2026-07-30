7a6099102823a9fe2f4c98f3dbb7a346d3d59730
GB-Navarro <gbnavarro@usp.br>
2026-07-30 16:09:01 -0300

    style: reestiliza telas de contas com o novo design system
    Corrige tambem cadastro_administrador.html, que nao tinha extends de
    base.html e por isso renderizava sem layout, css ou navbar nenhuma.
    Adiciona classes de widget nos forms para os inputs herdarem o estilo.

--- apps/contas/forms.py
+++ apps/contas/forms.py
@@ -3,15 +3,15 @@ from django import forms
 from apps.contas.models import Aluno, Usuario
 
 class CadastroAlunoForm(forms.Form):
-    nome = forms.CharField(max_length=200)
-    email = forms.EmailField()
-    senha = forms.CharField(widget=forms.PasswordInput)
-    genero = forms.ChoiceField(choices=Aluno.GENERO_CHOICES)
-    serie = forms.ChoiceField(choices=Aluno.SERIE_CHOICES)
-    escola = forms.CharField(max_length=200)
-    tipo_escola = forms.ChoiceField(choices=Aluno.TIPO_ESCOLA_CHOICES)
-    professor_nome = forms.CharField(max_length=200, required=False)
-    professor_email = forms.EmailField(required=False)
+    nome = forms.CharField(max_length=200, widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    senha = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    genero = forms.ChoiceField(choices=Aluno.GENERO_CHOICES, widget=forms.Select(attrs={"class": "form-select"}))
+    serie = forms.ChoiceField(choices=Aluno.SERIE_CHOICES, widget=forms.Select(attrs={"class": "form-select"}))
+    escola = forms.CharField(max_length=200, widget=forms.TextInput(attrs={"class": "form-control"}))
+    tipo_escola = forms.ChoiceField(choices=Aluno.TIPO_ESCOLA_CHOICES, widget=forms.Select(attrs={"class": "form-select"}))
+    professor_nome = forms.CharField(max_length=200, required=False, widget=forms.TextInput(attrs={"class": "form-control"}))
+    professor_email = forms.EmailField(required=False, widget=forms.EmailInput(attrs={"class": "form-control"}))
 
     def clean_email(self):
         email = self.cleaned_data["email"]
@@ -20,9 +20,9 @@ class CadastroAlunoForm(forms.Form):
         return email
 
 class CadastroAdministradorForm(forms.Form):
-    nome = forms.CharField(max_length=200)
-    email = forms.EmailField()
-    senha = forms.CharField(widget=forms.PasswordInput)
+    nome = forms.CharField(max_length=200, widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    senha = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}))
 
     def clean_email(self):
         email = self.cleaned_data["email"]
